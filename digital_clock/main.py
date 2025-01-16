06f2dd6927322f29bca46c8f28604282964e95fd
Ghafoor Nazari <GhafoorNazari@proton.me>
2025-01-16 17:16:32 +0100

    updated digital clock

--- digital_clock/main.py
+++ digital_clock/main.py
@@ -2,16 +2,16 @@ import tkinter as tk
 from time import strftime
 
 def time():
-    current_time = strftime('%H:%M:%S %p')
+    current_time = strftime('%H:%M:%S: %p')
     label.config(text=current_time)
     label.after(1000, time)
 
 root = tk.Tk()
 root.title("Digital Clock")
-root.geometry('500x250')
-root.config(bg="#2E3B55")
+root.geometry('500x200')
+root.config(bg="black")
 
-label = tk.Label(root, font=('Segoe UI', 50, 'bold'), background="#2E3B55", foreground="#00FF00")
+label = tk.Label(root, font=('Arial', 50, 'bold'),bg="black", fg="#00FF00")
 label.pack(anchor='center', pady=60)
 
 time()
