8b2617ba0644b2b9fdfe40b1ef46b6da9febf136
Ghafoor Nazari <GhafoorNazari@proton.me>
2025-01-22 16:37:28 +0100

    finished main.py(CLI)

--- weather_app/main.py
+++ weather_app/main.py
@@ -1,6 +1,38 @@
 import os
 import requests
-from python_dotenv import load_dotenv
+import json
+from dotenv import load_dotenv
 
 load_dotenv()
-api_key = os.dotenv("API_KEY")
+api_key = os.getenv("API_KEY")
+
+base_url = "https://api.openweathermap.org/data/2.5/weather?"
+
+print("Welcome to weather app. ")
+print("Enter name of a city. ")
+city_name = input(">>> ").capitalize()
+
+url = f"https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={api_key}&units=metric"
+
+response = requests.get(url)
+x = response.json()
+
+if x["cod"] == 200:
+    main = x["main"]
+    more_info = x["weather"]
+    
+    temperature = f"Temperature: {main["temp"]}C"
+    humidity = f"Humidity: {main["humidity"]}%"
+    detail = f"Description: {more_info[0]["description"]}"
+    
+    print(
+        f"Weather in {city_name}" +
+        "\n" +
+        temperature +
+        "\n" +
+        humidity +
+        "\n" +
+        detail)
+    
+else:
+    print("City not found. ")
