e8889fa9fb18b237dcf4544775ea9c4607da1091
Ghafoor Nazari <GhafoorNazari@proton.me>
2025-01-22 16:14:03 +0100

    writing weather app

--- weather_app/main.py
+++ weather_app/main.py
@@ -1,13 +1,6 @@
-from tkinter import *
+import os
+import requests
+from python_dotenv import load_dotenv
 
-root = Tk()
-root.geometry("500x500")
-root.title("Weather App")
-
-title_label = Label(root, text="Enter a city name!")
-title_label.grid(row=1, column=0, columnspan=5)
-
-city_input = Entry(root)
-city_input.grid(row=2, column=0, columnspan=5)
-
-root.mainloop()
+load_dotenv()
+api_key = os.dotenv("API_KEY")
