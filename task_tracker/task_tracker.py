0fabf29ea5bbc2e6d7069a1c6e1c3e444a9405be
Ghafoor Nazari <GhafoorNazari@proton.me>
2025-02-02 22:23:54 +0100

    Added task_tracker

---
+++ task_tracker/task_tracker.py
@@ -0,0 +1,10 @@
+command = input(">>> ")
+
+
+match command:
+    case "add":
+        print("adding task...")
+    case "edit":
+        print("editing task...")
+    case "done":
+        print("removing task...")
